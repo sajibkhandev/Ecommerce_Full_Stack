940a71b23e0d75e1e63b46b526100d93c9a25812
sajibkhandev <sajibkhan.mern@gmail.com>
2024-03-04 07:55:30 +0600

    helper for emailRegex

--- Backend/controllers/registrationController.js
+++ Backend/controllers/registrationController.js
@@ -1,4 +1,5 @@
 const blackInput = require("../helpers/blankInput");
+const emailRegex = require("../helpers/emailRegex");
 
 const registrationController=(req,res)=>{
    let {username,email,password}=req.body
@@ -8,7 +9,10 @@ const registrationController=(req,res)=>{
         res.send({error:"Name is required"})
     }else if(blackInput(email)){
         res.send({error:"Email is required"})
-    }else if(blackInput(password)){
+    }else if(emailRegex(email)){
+        res.send({error:"Valid email Required"})
+    }
+    else if(blackInput(password)){
         res.send({error:"Password is required"})
     }else{
         res.send(req.body)
---
+++ Backend/helpers/emailRegex.js
@@ -0,0 +1,9 @@
+const emailRegex=(email)=>{
+    let pattran=/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    
+    if(email){
+        return !(pattran.test(email))
+    }
+
+}
+module.exports=emailRegex
