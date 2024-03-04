cf9008926d69cb5ab4e98199640d24d9375ef4df
sajibkhandev <sajibkhan.mern@gmail.com>
2024-03-04 07:46:52 +0600

    helper for blankInput

--- Backend/controllers/registrationController.js
+++ Backend/controllers/registrationController.js
@@ -1,5 +1,18 @@
+const blackInput = require("../helpers/blankInput");
+
 const registrationController=(req,res)=>{
-    console.log(req.body);
+   let {username,email,password}=req.body
+
+    
+    if(blackInput(username)){
+        res.send({error:"Name is required"})
+    }else if(blackInput(email)){
+        res.send({error:"Email is required"})
+    }else if(blackInput(password)){
+        res.send({error:"Password is required"})
+    }else{
+        res.send(req.body)
+    }
 
 
     
---
+++ Backend/helpers/blankInput.js
@@ -0,0 +1,10 @@
+const blackInput=(text)=>{
+    if(!text){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+module.exports=blackInput
