b22e7ddd8bf92d2b36e3fb4bfa4f623a47cc7ff0
sajibkhandev <sajibkhan.mern@gmail.com>
2024-03-05 05:39:49 +0600

    opt route change

--- Backend/controllers/otpController.js
+++ Backend/controllers/otpController.js
@@ -2,8 +2,8 @@ const User=require('../models/userSchema')
 const otpController=async(req,res)=>{
 
     let {email,otp}=req.body
-    let user= await User.findOne({email:email})
-    if(user.otp==otp){
+    let existinguser= await User.findOne({email:email})
+    if(existinguser.otp==otp){
         await User.findOneAndUpdate({email:email},{otp:""})
         res.send({succee:"Veryfied"})
 
--- Backend/routes/api/registrationRoute.js
+++ Backend/routes/api/registrationRoute.js
@@ -5,6 +5,6 @@ const otpController = require('../../controllers/otpController')
 const _=express.Router()
 
 _.post("/registration",registrationMiddlerwar, registrationController)
-_.post("/otpRoute", otpController)
+// _.post("/otpRoute", otpController)
 
 module.exports=_
